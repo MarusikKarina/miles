1d375caf1b63f6e7596a72c20e45ba6e8dc9eb28
Karina Marusik <kamarusik@sberbank.ru>
2023-05-17 17:15:36 +0300

    bonus miles app

---
+++ src/ru/netology/kmarusik/Main.java
@@ -0,0 +1,11 @@
+package ru.netology.kmarusik;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int oneMilePrice = 20;
+        int ticketPrice = 1840;
+        int bonusMiles = ticketPrice / oneMilePrice;
+        System.out.println("bonus miles: " + bonusMiles);
+    }
+}
